298d0f90e3b1085c852558e64d78b5bbab7926ca
Eric Jiang <ericjiang107@gmail.com>
2021-12-15 20:36:23 -0500

    weather project using openweather api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
+<style>
+  @import './assets/weather.css';
+</style>
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="searchBox" id="main">
+      <!-- v-model takes the info stored in data and binds those two together -->
+      <!-- keypress is pinned to a method in the methods section -->
+      <input class="searchBar" type="text" placeholder="Enter Name" v-model="input" 
+      @keydown.enter="fetchWeather" />
+      <!-- v-for="example in weatherTest" -->
+      <!-- test to showcase what input is below -->
+      <!-- {{ input }} -->
+
+      <div class="weatherWrapper"> 
+
+        <div class="locationBox">
+
+          <div class="location" >
+            {{ weatherTest }}
+          </div>
+
+          <div class="date">
+            Date Test
+          </div>
+
+          <div class="weatherBox">
+
+            <div class="temperature">
+              Temp Test
+            </div>
+
+            <div class="weather">
+              Weather Test
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  // data setting:
+  data() {
+    return {
+      // setting in api key:
+      apiKey: '2efa223f9ef044586ce8f83ccca64d0b',
+      // when making requests, this is what you need to include in order to make it 
+      url_base: "api.openweathermap.org/data/2.5/",
+      input: '',
+      // something that stores the requests 
+      weatherTest: []
+    };
+  },
+
+  methods: {
+    fetchWeather() {
+      fetch(`${this.url_base}weather?q=${this.input}&appid=${this.apiKey}`)
+      .then(res => {
+        // res.clone().json()
+        // troubleshooting
+        console.log(res.json())
+      })
+      .then(data => { 
+        this.weatherTest = data
+        console.log("data: " + this.weatherTest)
+        })
+      .catch(err => console.log("there exists an error: " + err.message))
+    }
+
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
